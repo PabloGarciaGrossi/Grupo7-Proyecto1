d4ff7172711dcfefcbf66f46275001711f59725f
PabloGarciaGrossi <pabgar20@ucm.es>
2018-04-17 19:23:27 +0200

    Añadido mosquito

---
+++ Assets/Scripts/MosquitoIA.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MosquitoIA : MonoBehaviour {
+	PlayerController player;
+	public float speed;
+	public float playerRange;
+	public LayerMask playerlayer;
+	bool playerInRange;
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<PlayerController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerInRange = Physics2D.OverlapCircle (transform.position, playerRange, playerlayer);
+		if (playerInRange) {
+			transform.position = Vector3.MoveTowards (transform.position, player.transform.position, speed * Time.deltaTime);
+			if (player.transform.position.x < transform.position.x)
+				transform.localScale = new Vector3 (-2f, 2f, 1f);
+			else
+				transform.localScale = new Vector3 (2f, 2f, 1f);
+		}
+	}
+	void OnDrawGizmosSelected(){
+		Gizmos.DrawWireSphere (transform.position, playerRange);
+	}
+}
--- Assets/Scripts/VidaEnemigo.cs
+++ Assets/Scripts/VidaEnemigo.cs
@@ -12,13 +12,15 @@ public class VidaEnemigo : MonoBehaviour {
 	float iniciox, inicioy;
 	// Use this for initialization
 	void Start () {
-		if (gameObject.tag != "Oruga") {
+		if (gameObject.tag == "Rata") {
 			vel = GetComponent<Rigidbody2D> ().velocity;
-			gameObject.transform.GetChild (3).gameObject.SetActive (false);
+			gameObject.transform.GetChild (0).gameObject.SetActive (false);
+		} else if (gameObject.tag == "Oruga") {
+			speed = GetComponentInChildren<Oruga> ().speed;
+			anim = GetComponent<Animator> ();
 		}
-		else
-			speed = GetComponentInParent<Oruga> ().speed;
-		anim = GetComponent<Animator> ();
+		else if (gameObject.tag == "Mosquito")
+			speed = GetComponent<MosquitoIA> ().speed;
 		iniciox = transform.position.x;
 		inicioy = transform.position.y;
 		vidaRestante = maxVida;
@@ -26,17 +28,20 @@ public class VidaEnemigo : MonoBehaviour {
 	public void Damage()
 	{
 		if (gameObject.tag != "Oruga")
-		gameObject.transform.GetChild (3).gameObject.SetActive (true);
+		gameObject.transform.GetChild (0).gameObject.SetActive (true);
 		vidaRestante -= dmg;
 		if (vidaRestante <= 0) {
-			if (gameObject.tag == "Rata")
+			if (gameObject.tag == "Rata") {
 				GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
-			else if (gameObject.tag == "Oruga") {
+			}else if (gameObject.tag == "Oruga") {
 				gameObject.GetComponentInChildren<Oruga> ().speed = 0;
 				anim.SetBool ("Muerte", true);
+			} else if (gameObject.tag == "Mosquito") {
+				gameObject.GetComponent<MosquitoIA> ().speed = 0;
 			}
 			Invoke ("Destruir", 1f);
 		}
+		if (gameObject.tag != "Oruga")
 		Invoke ("DesactivaParticulas", 1);
 	}
 	public void Reset(){
@@ -45,6 +50,8 @@ public class VidaEnemigo : MonoBehaviour {
 			GetComponent<Rigidbody2D> ().velocity = vel;
 		else if (gameObject.tag == "Oruga")
 			gameObject.GetComponentInChildren<Oruga> ().speed = speed;
+		else if (gameObject.tag == "Mosquito")
+			gameObject.GetComponent<MosquitoIA> ().speed = speed;
 		vidaRestante = maxVida;
 		gameObject.transform.position = new Vector2 (iniciox, inicioy);
 		//gameObject.GetComponentInChildren<FieldOfViewEnemy> ().Detectado = false;
@@ -53,6 +60,6 @@ public class VidaEnemigo : MonoBehaviour {
 		gameObject.SetActive (false);
 	}
 	void DesactivaParticulas(){
-		gameObject.transform.GetChild (3).gameObject.SetActive (false);
+		gameObject.transform.GetChild (0).gameObject.SetActive (false);
 	}
 }
