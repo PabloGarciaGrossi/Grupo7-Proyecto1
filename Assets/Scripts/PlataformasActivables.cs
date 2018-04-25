e2cdb9862d72e2660faa8b2e1cfdf2cdbf5cdb15
PabloGarciaGrossi <pabgar20@ucm.es>
2018-04-25 10:42:11 +0200

    Continuacion nivel 1, añadidos puzles

--- Assets/Scripts/PlataformasActivables.cs
+++ Assets/Scripts/PlataformasActivables.cs
@@ -20,11 +20,11 @@ public class PlataformasActivables : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (activador.GetComponent<Palanca> ().palanca)
-			activado = activador.GetComponent<Palanca> ().activado;
-		else
-			activado = activador.GetComponent<Boton> ().activado;
-		if (activado) {
+			if (activador.GetComponent<Palanca> ().palanca)
+				activado = activador.GetComponent<Palanca> ().activado;
+			else
+				activado = activador.GetComponent<Boton> ().activado;
+	if (activado) {
 			platform.transform.position = Vector3.MoveTowards (platform.transform.position, currentpoint.position, Time.deltaTime * speed);
 			if (platform.transform.position == currentpoint.position) {
 				pointselection++;
