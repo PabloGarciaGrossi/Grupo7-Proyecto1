4e4e645dfa5695c8302ccfb9b6d3d592cd8ad58a
PabloGarciaGrossi <pabgar20@ucm.es>
2018-05-23 14:26:11 +0200

    Muerte boss actualizada

--- Assets/Scripts/TorreEléctrica.cs
+++ Assets/Scripts/TorreEléctrica.cs
@@ -27,13 +27,22 @@ public class TorreEléctrica : MonoBehaviour {
 			player.transform.position = new Vector3 (boss.transform.position.x, boss.transform.position.y - 5, 1);
 			camera.orthographicSize = 35;
 			player.gameObject.SetActive (false);
+			sonidoBoss.Play ();
 			Invoke ("ActivamasTarde", 2.5f);
+			InvokeRepeating ("Bajaintensidadboss", 6f, 0.1f);
 		}
 	}
 	void ActivamasTarde()
 	{
 		for (int i = 0; i < particulasdaño.Length; i++)
 			particulasdaño [i].gameObject.SetActive (true);
-		sonidoBoss.Play ();
+	}
+	void Bajaintensidadboss()
+	{
+		Color col = boss.GetComponent<SpriteRenderer> ().color;
+		boss.GetComponent<SpriteRenderer> ().color = new Color (col.r, col.g, col.b, col.a - Time.deltaTime/2);
+		foreach (GameObject rayo in electricidad) {
+			rayo.SetActive (false);
+		}
 	}
 }
